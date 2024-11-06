1c9d6bc6d9309432f5b8f8396e77890c353bd967
westonludeke <westonludeke@gmail.com>
2024-11-06 15:07:16 -0600

    reset project

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css';
-import Registration from './components/Registration';
-import Login from './components/Login';
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <h1>Discipline Tracker</h1>
-
-        <Routes>
-          <Route path="/register" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<h2>Welcome to Discipline Tracker</h2>} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    <>
+      <h1>discipline-tracker-app</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
